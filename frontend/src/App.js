f312b5ad3468affc7dfd07b821d56d02c8996823
sashbeloov <beloovsasha23@gmail.com>
2025-09-02 19:44:15 +0500

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,27 @@ import {Routes,Route} from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Create from './components/Create';
+import NavBar from './components/NavBar';
+
 
 
 
 
 function App() {
+  const myWidth = 200
   return (
     <div className="App">
-      <Routes>
-        <Route path='' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/create' element={<Create/>}/>
-
-      </Routes>
+      <NavBar 
+        drawerWidth={myWidth}
+        content = {
+          <Routes>
+            <Route path='' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/create' element={<Create/>}/>
+        </Routes>
+        }
+      />
+      
     </div>
   );
 }
