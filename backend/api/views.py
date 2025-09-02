b817c1454ee13f6ff0a1727f2dcdd1ee1b1c7ea7
sashbeloov <beloovsasha23@gmail.com>
2025-09-03 00:08:48 +0500

    added serializers

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Project
 
-# Create your views here.
+class ProjectViewSet(viewsets.Viewset):
+    permission_classes = [permissions.Allowany]
+    queryset = Project.objects.all()
+
+    def list(self, request):
+        pass
+    
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass
+
+
+    def update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000/'
+    'http://localhost:3000'
 ]
 
 ROOT_URLCONF = 'core.urls'
